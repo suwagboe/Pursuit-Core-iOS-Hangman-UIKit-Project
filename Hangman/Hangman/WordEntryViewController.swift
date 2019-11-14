ba502bc2b2a3310c0dbd7a0e3c4f405ab0048230
shaniyaUwagboe <shaniyauwagboe@pursuit.org>
2019-11-14 16:38:02 -0500

    added the outlets to gain access to variables

--- Hangman/Hangman/WordEntryViewController.swift
+++ Hangman/Hangman/WordEntryViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class WordEntryViewController: UIViewController {
 
+    //MARK: OUTLETS AND PROPERTIES
+    
+    //@IBOutlet var TextLabel: UIView!
+    
+    @IBOutlet weak var TextLabel: UILabel!
+    
+    
+    @IBOutlet weak var TextField: UITextField!
+    
+    @IBOutlet weak var confirmationButton: UIButton!
+    
   override func viewDidLoad() {
     super.viewDidLoad()
+    TextLabel.text = "WELCOME TO ZOMBIE HANGMAN!!!! PLAYER1 will enter in a word and PLAYER2 will have the chance to guess what word it is!! Below player1 should enter the word"
     // Do any additional setup after loading the view, typically from a nib.
   }
 
+    //MARK: Actions and functions make impacts
+    
+    
 
 }
 
