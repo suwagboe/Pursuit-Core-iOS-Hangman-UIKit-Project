d4d505b0d6f784428011f1e675dde30ef32fcf3c
shaniyaUwagboe <shaniyauwagboe@pursuit.org>
2019-11-14 17:18:58 -0500

    keyboard shows up

--- Hangman/Hangman/WordEntryViewController.swift
+++ Hangman/Hangman/WordEntryViewController.swift
@@ -16,20 +16,56 @@ class WordEntryViewController: UIViewController {
     
     @IBOutlet weak var TextLabel: UILabel!
     
-    
-    @IBOutlet weak var TextField: UITextField!
+    @IBOutlet weak var WordTextField: UITextField!
     
     @IBOutlet weak var confirmationButton: UIButton!
     
+    // opening actions
   override func viewDidLoad() {
     super.viewDidLoad()
     TextLabel.text = "WELCOME TO ZOMBIE HANGMAN!!!! PLAYER1 will enter in a word and PLAYER2 will have the chance to guess what word it is!! Below player1 should enter the word"
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    WordTextField.delegate = self
   }
 
     //MARK: Actions and functions make impacts
-    
-    
 
 }
 
+extension WordEntryViewController: UITextFieldDelegate {
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+           //if they want start typing in number and other things then it stops working
+        
+        
+        return true
+    }
+    
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        let myString = "abcdefghijklmnopqrstuvwxyz"
+        
+        for char in WordTextField.text ?? "a" {
+        if WordTextField.text?.contains(myString) ?? true {
+            textField.textColor = .green
+            TextLabel.text = " If you are all done click the button below"
+        } else {
+            TextLabel.text = "please!!!! enter in a word "
+            textField.textColor = .yellow
+        }
+        
+        }
+            // returns BOOL
+            
+        
+        //cancels out the keyboard.
+            
+        WordTextField.resignFirstResponder()
+        
+        //want for the text
+        return true
+    }
+    
+}
