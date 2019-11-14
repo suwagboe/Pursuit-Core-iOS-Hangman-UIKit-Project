d2b32263ba035125f2fad6baa21205101c0b966a
shaniyaUwagboe <shaniyauwagboe@pursuit.org>
2019-11-14 16:14:30 -0500

    readded the two story boards and fixed it so that way there is a navigation controller

---
+++ Hangman/Hangman/ConfirmsWordViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ConfirmsWordViewController.swift
+//  Hangman
+//
+//  Created by World Domination a Brand on 11/14/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmsWordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- Hangman/Hangman/WordEntryViewController.swift
+++ Hangman/Hangman/WordEntryViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WordEntryViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
